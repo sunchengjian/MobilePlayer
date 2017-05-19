94e679cb12b6a9ff23ba03a686b2561b036265a7
sunchengjian <3230706893@qq.com>
2017-05-19 13:01:16 +0800

    第一天上午的课程

---
+++ app/src/main/java/com/sunchengjian/mobileplayer/pager/NetAudioPager.java
@@ -0,0 +1,28 @@
+package com.sunchengjian.mobileplayer.pager;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.sunchengjian.mobileplayer.fragment.BaseFragment;
+
+public class NetAudioPager extends BaseFragment {
+    private TextView textView;
+
+    //重写视图
+    @Override
+    public View initView() {
+        textView = new TextView(context);
+        textView.setTextSize(30);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("网络音乐的内容");
+    }
+}
